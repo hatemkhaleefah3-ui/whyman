89d1813879ffb3dc7a8fe45a27c34edddd12c3ca
hatemkhaleefah3-ui <hatemkhaleefah3@gmail.com>
2026-08-05 05:39:28 +0300

    Use complete navigation page experience

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import { NavExperience } from './NavExperience';
 import './styles.css';
 import './redesign.css';
 import './mobile.css';
+import './nav-experience.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <NavExperience />
   </React.StrictMode>,
 );

406bcd54c9b6cd6014f84c39afbcae4fd31f13db
hatemkhaleefah3-ui <hatemkhaleefah3@gmail.com>
2026-08-05 05:22:58 +0300

    Load mobile-first bottom navigation redesign

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './styles.css';
 import './redesign.css';
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
+import './mobile.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);

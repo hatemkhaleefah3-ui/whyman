df24c1416140a54d2bac99e01092e47cb818f56e
hatemkhaleefah3-ui <hatemkhaleefah3@gmail.com>
2026-08-05 08:15:38 +0300

    Launch production account flow

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BackendGate} from './BackendGate';
+import {ProductionApp} from './ProductionApp';
 import './styles.css';
 import './redesign.css';
 import './mobile.css';
 import './nav-experience.css';
 import './backend-status.css';
+import './production-app.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BackendGate />
+    <ProductionApp />
   </React.StrictMode>,
 );
